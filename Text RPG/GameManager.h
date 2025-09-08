d7a5069a86e926284b5fd4bcff8d54b0853ef3e2
wkej <wjk5413@naver.com>
2025-09-08 16:11:47 +0900

    feat
    다중 경험치를 위한 다중 몬스터 전투 기능이 추가되었습니다. 플레이어보다 1레벨 낮거나 같은 레벨의 몬스터 다수가 일정 확률로 다수 줄현합니다. 대략 2-4마리가 출현합니다. 수정 방향성을 알려주시면 감사하겠습니다.

--- Text RPG/GameManager.h
+++ Text RPG/GameManager.h
@@ -5,14 +5,17 @@
 #include "Character.h"
 #include "Monster.h"
 #include "Goblin.h"
-#include "Troll.h";
-#include "Orc.h";
-#include "Slime.h";
+#include "Troll.h"
+#include "Orc.h"
+#include "Slime.h"
+#include <vector>
 
-class GameManager{
+class GameManager {
 public:
 	Monster* generateMonster(int level);
+	std::vector<Monster*> generateMultipleMonsters(int playerLevel);  // 추가
 	void battle(Character* Player);
+	void multiBattle(Character* Player, std::vector<Monster*>& monsters);  // 추가
 	void displayInventory(Character* Player);
 	void reward(Character* Player, int exp);
 	int randNum(int min, int max);
