665d6a32b3da8d2b54d3abd6437c17ebb8fcdc8c
Daeho <84126277+HyukiKR@users.noreply.github.com>
2025-09-10 17:08:20 +0900

    Revert "Merge branch 'game_manager' into develop"
    This reverts commit c199ceb336c6856a2208113385302755bd2f2853, reversing
    changes made to e0c532e265f0171fe3db753d9b2fe0f4044adee0.

--- Text RPG/GameManager.h
+++ Text RPG/GameManager.h
@@ -21,10 +21,9 @@
 class GameManager {
 public:
 	Monster* generateMonster(int level);
-	std::vector<Monster*> generateMultipleMonsters(int playerLevel);
+	std::vector<Monster*> generateMultipleMonsters(int playerLevel);  // 추가
 	void battle(Character* Player);
-	void multiBattle(Character* Player, std::vector<Monster*>& monsters);
-	void handleAfterBattle(Character* Player);  // 새로 추가
+	void multiBattle(Character* Player, std::vector<Monster*>& monsters);  // 추가
 	void displayInventory(Character* Player);
 	void reward(Character* Player, int exp);
 	int randNum(int min, int max);
